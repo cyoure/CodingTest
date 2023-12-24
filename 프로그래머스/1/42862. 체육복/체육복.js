8f2618c5cbc2e4c66f3fd891d6c1e555d67ea027
cyoure <152484982+cyoure@users.noreply.github.com>
2023-12-24 19:57:37 +0900

    [level 1] Title: 체육복, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,25 +1,29 @@
 function solution(n, lost, reserve) {
-    const students = {};
-    let answer = 0;
-    for(let i = 1; i <= n; i++){
-        students[i] = 1;
+  let total = [];
+  let answer = 0;
+  for (let i = 0; i < n; i++) {
+    total[i] = 1;
+  }
+  lost.forEach((data) => (total[data - 1] -= 1));
+  reserve.forEach((data) => (total[data - 1] += 1));
+  for (let i = 0; i < total.length; i++) {
+    if (total[i] === 2 && total[i - 1] === 0) {
+      total[i - 1]++;
+      total[i]--;
+    } else if (total[i] === 2 && total[i + 1] === 0) {
+      total[i]--;
+      total[i + 1]++;
     }
-    lost.forEach(number => students[number] -= 1);
-    reserve.forEach(number => students[number] += 1);
-
-    for(let i = 1; i <= n; i++){
-        if(students[i] === 2 && students[i-1] === 0){
-                students[i-1]++;
-                students[i]--;
-        } else if(students[i] === 2 && students[i+1] === 0){
-                students[i+1]++;
-                students[i]--;
-        }
+  }
+  for (let i = 0; i < total.length; i++) {
+    if (total[i] >= 1) {
+      answer++;
     }
-    for(let key in students){
-        if(students[key] >= 1){
-            answer++;
-        }
-    }
-    return answer;
+  } return answer;
 }
+
+// 전체 학생을 배열로 만들어 모두 1값을 가지게 함
+// 잃어버린 친구들은 요소 -1
+// 여별은 +1
+// 앞에서 여벌을 체육복을 받을지 뒤에서 받을지 정해주기
+// 다 나누어 준 다음 배열의 요소의 값이 1이상인 수를 구해서 리턴해주기
