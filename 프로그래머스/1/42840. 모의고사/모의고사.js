604486df1340e99825b175bfcacb950a0abdcfcc
cyoure <152484982+cyoure@users.noreply.github.com>
2024-06-27 14:22:17 +0900

    [level 1] Title: 모의고사, Time: 102.14 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -6,33 +6,62 @@
 // 수포자 3 : 3 3 1 1 2 2 4 4 5 5 / 3 3 1 1 2 2 4 4 5 5
 // 정답지 answers -> 배열
 function solution(answers) {
+  let answer = [];
+  let newsupo1 = [];
+  let newsupo2 = [];
+  let newsupo3 = [];
   let supo1 = [1, 2, 3, 4, 5];
   let supo2 = [2, 1, 2, 3, 2, 4, 2, 5];
   let supo3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-  let key = [0, 0, 0];
-  let value = [];
-  for(let i =0; i< answers.length; i++) {
-    if(supo1[i%supo1.length] === answers[i]) {
-      key[0]++;
+  let num = [0, 0, 0];
+
+  newsupo1 = newsupo1.concat(supo1);
+  newsupo2 = newsupo2.concat(supo2);
+  newsupo3 = newsupo3.concat(supo3);
+  if (answers.length > 5) {
+    for (let i = 0; i < answers.length / 5 - 1; i++) {
+      newsupo1 = newsupo1.concat(supo1);
+      newsupo2 = newsupo2.concat(supo2);
+      newsupo3 = newsupo3.concat(supo3);
     }
   }
-  for(let i =0; i< answers.length; i++) {
-    if(supo2[i%supo2.length] === answers[i]) {
-      key[1]++;
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === newsupo1[i]) {
+      num[0]++;
+    }
+    if (answers[i] === newsupo2[i]) {
+      num[1]++;
+    }
+    if (answers[i] === newsupo3[i]) {
+      num[2]++;
     }
   }
-  for(let i =0; i< answers.length; i++) {
-    if(supo3[i%supo3.length] === answers[i]) {
-      key[2]++;
+
+  let max = num[0];
+  max = max < num[1] ? num[1] : num[0];
+  max = max < num[2] ? num[2] : max;
+
+  if (max === num[0]) {
+    answer = [1];
+    if (max === num[1]) {
+      answer = [1, 2];
     }
-  }
-   let big = Math.max(key[0], key[1], key[2]);
-  for (let i = 0; i < 3; i++) {
-    if(key[i] === big){
-    value.push(i+1);
+    if (max === num[2]) {
+      answer = [1, 3];
     }
+    if (max === num[1] && max === num[2]) {
+      answer = [1, 2, 3];
+    }
+  } else if (max === num[1]) {
+    answer = [2];
+    if (max === num[2]) {
+      answer = [2, 3];
+    }
+  } else if (max === num[2]) {
+    answer = [3];
   }
- return value;
+  return answer;
 }
 // 수포자들의 답을 배열에 넣는다
 // for문을 돌며 같은 값을 찾아 몇개 인지 확인
